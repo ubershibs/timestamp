fdca53e04f3dbacdf1a374dda73eb89098821ba7
ubershibs <luke.walker@gmail.com>
2016-05-05 08:45:46 -0400

    Initial iteration of timestamp microservice

---
+++ index.js
@@ -0,0 +1,47 @@
+var express = require('express')
+var moment = require('moment')
+var fs = require('fs')
+var path =
+
+var app = express()
+
+port = porcess.env.PORT || 8000;
+
+app.get('/:url', function(req, res) {
+  var input = req.params.url;
+
+  if(/^\d{8}$/.test(input)) {
+    var parsed = moment(input, "X")
+  } else {
+    var parsed = moment(input, "MMMM D, YYYY")
+  }
+  if(parsed.isValid()) {
+    res.json({
+      unix: parsed.format("X"),
+      natural: parsed.format("MMMM D, YYYY")
+    });
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    })
+  }
+
+})
+
+
+app.get('/', function(req, res) {
+  var file = path.join(__dirname, 'index.html');
+  res.sendFile(file, function(err) {
+    if (err) {
+      console.log(err)
+      res.status(err.status).end()
+    } else {
+      console.log('Sent:', file)
+    }
+  })
+})
+
+app.listen(port, function() {
+  console.log("Listening on port: " + port);
+})
